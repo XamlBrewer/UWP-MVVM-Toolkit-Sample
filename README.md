d872caa2503cd8321438934ba58fcb1d39a46f89
Diederik Krols <diederik_krols@hotmail.com>
2020-07-03 20:48:07 +0200

    Ioc page: added Service Locator sample

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # UWP MVVM Toolkit Sample
 
 (Work in progress)
+
 (Currently references an CI build of the NuGet package)
 
 Demonstrates using the Windows Community MVVM Toolkit in a UWP app.
@@ -12,3 +13,5 @@ Demonstrates using the Windows Community MVVM Toolkit in a UWP app.
 ![Screenshot](Assets/BuildingblocksPage.png?raw=true)
 
 ![Screenshot](Assets/MessengerPage.png?raw=true)
+
+![Screenshot](Assets/InversionOfControlPage.png?raw=true)
--- XamlBrewer.UWP.MvvmToolkit.Sample/Views/InversionOfControlPage.xaml.cs
+++ XamlBrewer.UWP.MvvmToolkit.Sample/Views/InversionOfControlPage.xaml.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.Toolkit.Mvvm.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using Windows.UI.Xaml.Controls;
+using XamlBrewer.UWP.MvvmToolkit.Sample.Services.Logging;
 
 namespace XamlBrewer.UWP.MvvmToolkit.Sample.Views
 {
@@ -10,10 +10,15 @@ namespace XamlBrewer.UWP.MvvmToolkit.Sample.Views
     {
         public InversionOfControlPage()
         {
-            //Ioc.Default.ConfigureServices
-            //    (s => s.Add());
-
             this.InitializeComponent();
+        }
+
+        private void LogButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var loggingService = Ioc.Default.GetService<ILoggingService>();
+
+            // © Planet Trek.
+            loggingService.Log($"Captain's Log - Stardate {DateTime.Now} - A friendly lifeform clicked the 'Log' button.");
         }
     }
 }
